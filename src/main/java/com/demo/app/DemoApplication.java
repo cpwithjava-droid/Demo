92efebf2aa4eac4bd8504825ac52e5d1f80527e2
cpwithjava-droid <cpwithjava@gmail.com>
2025-12-15 10:41:23 +0530

    Update DemoApplication.java

--- src/main/java/com/demo/app/DemoApplication.java
+++ src/main/java/com/demo/app/DemoApplication.java
@@ -1,3 +1,11 @@
+package com.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 @RestController
 @RequestMapping("/api")
@@ -5,9 +13,6 @@ public class DemoApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
     private static final String APP_VERSION = "1.0.0";
-
-    // --- FAKE TOKEN FOR GITLEAKS TEST ---
-    private static final String FAKE_API_TOKEN = "ghp_1234567890abcdefTESTTOKEN";
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
